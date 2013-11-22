419850b5c0b8a4b0876b4a860e9a8cb590db5e50
Pete Blois <blois@google.com>
2013-11-22 10:46:11 -0800

    Adding an ES6 sample, using the traceur compiler.

--- demo/polymer-mixin.js
+++ demo/polymer-mixin.js
@@ -1,13 +1,16 @@
-function mixin(to, from) {
-  Object.getOwnPropertyNames(from).forEach(function(name) {
-    Object.defineProperty(to, name,
-        Object.getOwnPropertyDescriptor(from, name));
-  });
-  return to;
-};
 
-var PolymerMixin = mixin({}, Polymer.api.instance.base);
-mixin(PolymerMixin, Polymer.api.instance.attributes);
-mixin(PolymerMixin, Polymer.api.instance.properties);
-mixin(PolymerMixin, Polymer.api.instance.events);
-mixin(PolymerMixin, Polymer.api.instance.mdv);
+if (!Object.mixin) {
+  Object.mixin = function(to, from) {
+    Object.getOwnPropertyNames(from).forEach(function(name) {
+      Object.defineProperty(to, name,
+          Object.getOwnPropertyDescriptor(from, name));
+    });
+    return to;
+  }
+}
+
+var PolymerMixin = Object.mixin({}, Polymer.api.instance.base);
+Object.mixin(PolymerMixin, Polymer.api.instance.attributes);
+Object.mixin(PolymerMixin, Polymer.api.instance.properties);
+Object.mixin(PolymerMixin, Polymer.api.instance.events);
+Object.mixin(PolymerMixin, Polymer.api.instance.mdv);
